56ada19943e9ff427c39145745ac468caac36e78
lobsterbandit <code.lobsterbandit@outlook.com>
2020-10-24 19:50:03 -0400

    Scaffold app

---
+++ README.md
@@ -0,0 +1,3 @@
+# chess-trainer
+
+Play and learn chess.
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Chess Trainer</h1>
+      <div>Menu Bar</div>
+      <main>Board</main>
+    </div>
+  );
+}
+
+export default App;
